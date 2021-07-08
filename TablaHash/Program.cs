43260a65875a3acd9a41a1972681b15a32eef4b8
JOSE <josemchavez0123@hotmail.com>
2021-07-08 08:16:16 -0400

    Add project files.

---
+++ TablaHash/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace TablaHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo vehiculo1 = new Vehiculo { Codigo = "marTier", Marca = "Honda", Modelo = "Civid", Placa = "RD907843", Serie = "43443-43434-jkd32-2342",Ano=2003 };
+            Vehiculo vehiculo2 = new Vehiculo { Codigo = "212343", Marca = "Honda", Modelo = "Civid", Placa = "RD7843", Serie = "43443-43434-jkd32-2342", Ano = 2022 };
+            Vehiculo vehiculo3 = new Vehiculo { Codigo = "gr3434", Marca = "Honda", Modelo = "Civid", Placa = "RD9843", Serie = "43443-43434-jkd32-2342", Ano = 2003 };
+            TablaH tabla = new TablaH(5);
+            tabla.Insertar(vehiculo1);
+            tabla.Insertar(vehiculo2);
+            tabla.Insertar(vehiculo3);
+            tabla.Eliminar("212343");
+            tabla.Imprimir();
+            Console.WriteLine(tabla.BuscarVehiculoDelAno("gr3434").Serie);
+
+        }
+    }
+}
---
+++ TablaHash/Vehiculo.cs
@@ -0,0 +1,115 @@
+﻿
+
+using System;
+
+namespace TablaHash
+{
+    public class Vehiculo
+    {
+        public string Codigo { get; set; }
+        public string Modelo { get; set; }
+        public string Serie { get; set; }
+        public string Placa { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+      
+    }
+    public class TablaH
+    {
+        private int tamanoTabla;
+        protected Vehiculo[] Tabla { get; set; }
+        protected double FactorCarga { get; set; }
+        protected int NumeroElementos { get;set;}
+
+
+        public TablaH(int tamano)
+        {
+            tamanoTabla = tamano;
+            Tabla = new Vehiculo[tamano];
+            for (int i = 0; i < tamano; i++)
+            {
+                Tabla[i] = null;
+            }
+            NumeroElementos = 0;
+            FactorCarga = 0.0d;
+        }
+        /// <summary>
+        /// Devuelve la posicion donde se escuentre el vehiculo y si no existe te devuelve una posicion vacia
+        /// </summary>
+        /// <param name="clave"></param>
+        /// <returns></returns>
+        public int FuncionHash(string clave)
+        {
+            int i = 0;
+            long p, d;
+            d = TransformaCadena(clave);
+            p = d % tamanoTabla;
+            while (Tabla[p]!= null && !Tabla[p].Codigo.Equals(clave))
+            {
+                i++;
+                p += i * i;
+                p %= tamanoTabla;// Considera el array como circular
+            }
+            return (int)p;
+
+        }
+        public void Insertar(Vehiculo vehiculo)
+        {
+            int pos = FuncionHash(vehiculo.Codigo);
+            Tabla[pos] = vehiculo;
+            NumeroElementos++;
+            FactorCarga = (double)NumeroElementos / tamanoTabla;
+            if (FactorCarga>0.7d)
+            {
+                Console.WriteLine("El Factor de carga supera el 70%");
+                Console.WriteLine("\n Aumente el tamaño");
+            }
+        }
+        public Vehiculo BuscarVehiculoDelAno(string clave)
+        {
+            int pos = FuncionHash(clave);
+            Vehiculo v = Tabla[pos];
+            if (v != null)
+            {
+                if (v.Ano>=DateTime.UtcNow.Year)
+                {
+                    return v;
+                }
+            }
+            return new Vehiculo();
+        }
+        public Vehiculo Buscar(string clave)
+        {
+            int pos = FuncionHash(clave);
+            return Tabla[pos];
+
+        }
+
+        public void Eliminar(string clave)
+        {
+            int pos = FuncionHash(clave);
+            if (Tabla[pos] != null)
+            {
+                Tabla[pos]=null;
+            }
+        }
+        private long TransformaCadena(string clave)
+        {
+            long digitos = 0;
+            for (int i = 0; i < clave.Length; i++)
+            {
+                digitos = digitos * 27 + (int)clave[i];
+            }
+            return digitos;
+        }
+        public void Imprimir()
+        {
+            for (int i = 0; i < tamanoTabla; i++)
+            {
+                
+                if (Tabla[i] != null)
+                    Console.WriteLine($"Codigo:{Tabla[i].Codigo}-- Vehiculo: {Tabla[i].Marca} {Tabla[i].Modelo} {Tabla[i].Ano}-- Serie:{Tabla[i].Serie}--Matricula:{Tabla[i].Placa}");
+            }
+        }
+    }
+}
